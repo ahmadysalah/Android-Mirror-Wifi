1b4841b69ee6874bf1b62916a73ef39c2d9fe979
Ahmad Salah <a.ahmad.salah@gmail.com>
2021-09-03 23:30:34 +0300

    Mirror your android mobile using scrcpy

---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+// connect device and make sure enabeled the adb and wifi 
+// sadb also before excute the app
+
+const { exec } = require("child_process");
+const { setTimeout } = require("timers");
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+exec("adb disconnect", (error, data) => console.log(data))
+setTimeout(() => {
+    exec("adb shell ip route", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`insert usb`);
+            return;
+        }
+        if (stdout) {
+            try {
+                const deviceIp = stdout.split('src ')[1]
+                console.log(`Connecting:`, deviceIp);
+                exec("adb tcpip 5555", (err, data) => {
+                    console.log(data)
+                    exec(`adb connect  ${deviceIp}:5555`, (error, data) => {
+                        if (data) {
+                            console.log(data)
+                            console.log('Connecting .... ')
+                            exec("scrcpy")
+                        }
+                    })
+                })
+            } catch (error) {
+                console.log('Error try:')
+                console.log('1- unplug the cable')
+                console.log('1- insert the cable again')
+            }
+        }
+    });
+}, 2 * 1000)
+
+
